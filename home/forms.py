6894f9c9db539b0ff334f08c3fd47ee0657d058e
andruasha <anvolkov_1@edu.hse.ru>
2023-06-15 20:19:38 +0300

    update

--- home/forms.py
+++ home/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from home.models import Spells, Author
+from home.models import Bestiary, Items, Spells, Author
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
@@ -59,7 +59,58 @@ class UserRegistrationForm(UserCreationForm):
         fields = ('username', 'first_name', 'last_name', 'Organization', 'email', 'password1', 'password2')
 
 
-class SpellForm(forms.ModelForm):
+class BestiaryCreateForm(forms.ModelForm):
+    class Meta:
+        model = Bestiary
+        fields = ['Bestiary_ID', 'Size_ID', 'Species_ID',
+                  'Speed', 'Worldview_ID', 'Danger',
+                  'Bestiary_Author', 'Language_ID', 'Habitat_ID',
+                  'Hits', 'Armor_ID', 'Characteristics',
+                  'Resistance_Damage', 'Immunity_Damage', 'Skills',
+                  'Description', ]
+
+
+class BestiaryDeleteForm(forms.ModelForm):
+    class Meta:
+        model = Bestiary
+        fields = ['Bestiary_ID', 'Size_ID', 'Species_ID',
+                  'Worldview_ID', 'Danger',
+                  'Bestiary_Author', 'Language_ID', 'Habitat_ID',
+                  'Hits', 'Armor_ID', ]
+
+
+class BestiaryFindForm(forms.ModelForm):
+    class Meta:
+        model = Bestiary
+        fields = ['Bestiary_ID', 'Size_ID', 'Species_ID',
+                  'Worldview_ID', 'Danger',
+                  'Bestiary_Author', 'Language_ID', 'Habitat_ID',
+                  'Hits', 'Armor_ID', ]
+
+
+class ItemsCreateForm(forms.ModelForm):
+    class Meta:
+        model = Items
+        fields = ['Item_ID', 'Item_Type', 'Item_Rarity',
+                  'Item_Setting', 'Item_Author', 'Item_Price',
+                  'Item_Description', ]
+
+
+class ItemsDeleteForm(forms.ModelForm):
+    class Meta:
+        model = Items
+        fields = ['Item_ID', 'Item_Type', 'Item_Rarity',
+                  'Item_Setting', 'Item_Author', 'Item_Price', ]
+
+
+class ItemsFindForm(forms.ModelForm):
+    class Meta:
+        model = Items
+        fields = ['Item_ID', 'Item_Type', 'Item_Rarity',
+                  'Item_Setting', 'Item_Author', 'Item_Price', ]
+
+
+class SpellFindForm(forms.ModelForm):
     class Meta:
         model = Spells
         fields = ['Spell_ID', 'Spell_Level', 'Archetypes', 'Spell_Author']
--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
-from home.forms import SpellForm
+from home.forms import BestiaryFindForm, BestiaryDeleteForm, BestiaryCreateForm
+from home.forms import ItemsFindForm, ItemsDeleteForm, ItemsCreateForm
+from home.forms import SpellFindForm
 from django.shortcuts import render, HttpResponseRedirect
 from home.models import Author
 from home.forms import UserLoginForm, UserRegistrationForm
@@ -39,32 +41,135 @@ def index(request):
 
 
 def bestiary_find(request):
-    return render(request, 'home/bestiary_find.html')
+    if request.method == 'POST':
+        form = BestiaryCreateForm(data=request.POST)
+        Bestiary_ID = request.POST['Bestiary_ID']
+        Size_ID = request.POST['Size_ID']
+        Species_ID = request.POST['Species_ID']
+        Worldview_ID = request.POST['Worldview_ID']
+        Danger = request.POST['Danger']
+        Bestiary_Author = request.POST['Bestiary_Author']
+        Language_ID = request.POST['Language_ID']
+        Habitat_ID = request.POST['Habitat_ID']
+        Hits = request.POST['Hits']
+        Armor_ID = request.POST['Armor_ID']
+
+        print(Bestiary_ID)
+    else:
+        form = BestiaryFindForm()
+    authors = Author.objects.all()
+    context = {'form': form, 'authors': authors}
+    return render(request, 'home/bestiary_find.html', context)
 
 
 def bestiary_create(request):
-    return render(request, 'home/bestiary_create.html')
+    if request.method == 'POST':
+        form = BestiaryCreateForm(data=request.POST)
+        Bestiary_ID = request.POST['Bestiary_ID']
+        Size_ID = request.POST['Size_ID']
+        Species_ID = request.POST['Species_ID']
+        Speed = request.POST['Speed']
+        Worldview_ID = request.POST['Worldview_ID']
+        Danger = request.POST['Danger']
+        Bestiary_Author = request.POST['Bestiary_Author']
+        Language_ID = request.POST['Language_ID']
+        Habitat_ID = request.POST['Habitat_ID']
+        Hits = request.POST['Hits']
+        Armor_ID = request.POST['Armor_ID']
+        Characteristics = request.POST['Characteristics']
+        Resistance_Damage = request.POST['Resistance_Damage']
+        Immunity_Damage = request.POST['Immunity_Damage']
+        Skills = request.POST['Skills']
+        Description = request.POST['Description']
+
+        print(Bestiary_ID)
+    else:
+        form = BestiaryCreateForm()
+    authors = Author.objects.all()
+    context = {'form': form, 'authors': authors}
+    return render(request, 'home/bestiary_create.html', context)
 
 
 def bestiary_delete(request):
-    return render(request, 'home/bestiary_delete.html')
+    if request.method == 'POST':
+        form = BestiaryCreateForm(data=request.POST)
+        Bestiary_ID = request.POST['Bestiary_ID']
+        Size_ID = request.POST['Size_ID']
+        Species_ID = request.POST['Species_ID']
+        Worldview_ID = request.POST['Worldview_ID']
+        Danger = request.POST['Danger']
+        Bestiary_Author = request.POST['Bestiary_Author']
+        Language_ID = request.POST['Language_ID']
+        Habitat_ID = request.POST['Habitat_ID']
+        Hits = request.POST['Hits']
+        Armor_ID = request.POST['Armor_ID']
+
+        print(Bestiary_ID)
+    else:
+        form = BestiaryDeleteForm()
+    authors = Author.objects.all()
+    context = {'form': form, 'authors': authors}
+    return render(request, 'home/bestiary_delete.html', context)
 
 
 def items_find(request):
-    return render(request, 'home/items_find.html')
+    if request.method == 'POST':
+        form = ItemsCreateForm(data=request.POST)
+        Item_ID = request.POST['Item_ID']
+        Item_Type = request.POST['Item_Type']
+        Item_Rarity = request.POST['Item_Rarity']
+        Item_Setting = request.POST['Item_Setting']
+        Item_Author = request.POST['Item_Author']
+        Item_Price = request.POST['Item_Price']
+
+        print(Item_ID)
+    else:
+        form = ItemsFindForm()
+    authors = Author.objects.all()
+    context = {'form': form, 'authors': authors}
+    return render(request, 'home/items_find.html', context)
 
 
 def items_create(request):
-    return render(request, 'home/items_create.html')
+    if request.method == 'POST':
+        form = ItemsCreateForm(data=request.POST)
+        Item_ID = request.POST['Item_ID']
+        Item_Type = request.POST['Item_Type']
+        Item_Rarity = request.POST['Item_Rarity']
+        Item_Setting = request.POST['Item_Setting']
+        Item_Author = request.POST['Item_Author']
+        Item_Price = request.POST['Item_Price']
+        Item_Description = request.POST['Item_Description']
+
+        print(Item_ID)
+    else:
+        form = ItemsCreateForm()
+    authors = Author.objects.all()
+    context = {'form': form, 'authors': authors}
+    return render(request, 'home/items_create.html', context)
 
 
 def items_delete(request):
-    return render(request, 'home/items_delete.html')
+    if request.method == 'POST':
+        form = ItemsCreateForm(data=request.POST)
+        Item_ID = request.POST['Item_ID']
+        Item_Type = request.POST['Item_Type']
+        Item_Rarity = request.POST['Item_Rarity']
+        Item_Setting = request.POST['Item_Setting']
+        Item_Author = request.POST['Item_Author']
+        Item_Price = request.POST['Item_Price']
+
+        print(Item_ID)
+    else:
+        form = ItemsCreateForm()
+    authors = Author.objects.all()
+    context = {'form': form, 'authors': authors}
+    return render(request, 'home/items_delete.html', context)
 
 
 def spells_find(request):
     if request.method == 'POST':
-        form = SpellForm(data=request.POST)
+        form = SpellFindForm(data=request.POST)
         Spell_ID = request.POST['Spell_ID']
         Spell_Level = request.POST['Spell_Level']
         Archetypes = request.POST['Archetypes']
@@ -72,7 +177,7 @@ def spells_find(request):
 
         print(Spell_ID, Spell_Level, Archetypes, Spell_Author)
     else:
-        form = SpellForm()
+        form = SpellFindForm()
     authors = Author.objects.all()
     context = {'form': form, 'authors': authors}
     return render(request, 'home/spells_find.html', context)
